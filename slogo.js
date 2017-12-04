354c7fc41954bbeb858a3be9775cc5e3f9ab465a
lightsage88 <adrian.e.rosales@gmail.com>
2017-12-03 18:54:39 -0800

    got an api key for googlemaps

--- slogo.js
+++ slogo.js
@@ -1,1 +1,28 @@
 var x = 'hello world';
+// create a client , lets muggles do something valuable or interesting
+with with the API
+
+//SERVE STATIC FILES
+	You will create a server using Node.js
+	There will be a static folder for static assets
+	such as index.html, any images, fonts, and 
+	satellite pages
+
+//Create a RESTful API with all 4 crud operations
+// Create
+// Read
+// Update
+// Delete
+//obviously we can make something were clients
+// create user accounts, and can update them, read their
+// info, obviously, and then delete them.
+
+Your idea will define the scope of the RESTful API 
+you create.
+
+//Create and pass TESTS for the API endpoints. (using 
+// mocha and chai) USE MOCHA 3.5.5!!!
+//throw some edge cases in there too, for good measure
+
+
+//Use continuous integration: travisCI, heroku, and
