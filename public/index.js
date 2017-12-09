7c6dccbf6441262c0e00f07eabacdd966312f2e9
lightsage88 <adrian.e.rosales@gmail.com>
2017-12-09 00:23:49 -0800

    got CRD done, just need to get update in

--- public/index.js
+++ public/index.js
@@ -1,4 +1,8 @@
 
+let mLabApiKey = 'pJysmium6S33nXs_wxZ0VK9wyMIQQlSa';
+let mLabDb = 'mobius';
+let mLabCollection = 'hoomen';
+
 
 $('#registerForm').submit(function(event){
 	event.preventDefault();
@@ -21,8 +25,10 @@ $('#registerForm').submit(function(event){
 			console.log(response);
 			$('#registerForm').after('<p class="accountCreation">Your account was successfully created</p>');
 			$('.accountCreation').fadeOut(2000);
-			//insert reference to a function that will countdown to logging them in with
-			//their credentials.
+				(setTimeout(function(){
+					location.href = 'login.html';
+				}, 2000));
+			//fancy counting function that calls itself, WHUUUUT?!
 		},
 		error: (err) => {
 			console.log('god damnit');
@@ -39,7 +45,7 @@ $('#loginForm').submit(function(event){
 	let password = $('#password').val();
 	console.log('Batman never quits and neither should you!');
 	$.ajax({
-		//method url data contentType dataType success and error
+		//method url data contentType dataType success and error 
 		method: "POST",
 		url: "/api/cyberPolice/login",
 		data: JSON.stringify({username: username, password: password}),
@@ -51,20 +57,10 @@ $('#loginForm').submit(function(event){
 			console.log(username); 
 			localStorage.setItem('token', response.authToken);
 			toProtectedData();
-			getDataFromMlab(username);
-			 // location.href='accountPage.html';
-
-			//add something taking the user to the protected data stuff
-		},
-		// complete: function(){
-		// 	location.href='accountPage.html';
-		// },
-		error: (err) => {
-			console.log('Keep trying, homes');
-			console.error(err);
-			$('main').append(`${err.responseJSON.message}`);
-		}
+			getDataFromMlabXXXLoadAccountPage(username);
+		}//I read somewhere you couldn't have both a success and an error callback in the same request...one for Ray.
 	});
+	// location.href='accountPage.html';
 });
 
 function toProtectedData() {
@@ -78,7 +74,7 @@ function toProtectedData() {
 		authorization: `Bearer ${token}`
 	},
 		dataType: 'json',
-		success: function(response) {
+		done: function(response) {
 			console.log(response.data)
 		},
 		error: (err) => {
@@ -92,14 +88,12 @@ function toProtectedData() {
 
 //to make get stuff from the database to populate our span tags and
 //unordered lists username name bookList
- $('.username').html(sessionStorage.firstName);
-// $('.username').html();
+ $('.firstName').html(localStorage.firstName);
+ $('.username').html(localStorage.username);
 
-let mLabApiKey = 'pJysmium6S33nXs_wxZ0VK9wyMIQQlSa';
-let mLabDb = 'mobius';
-let mLabCollection = 'hoomen';
 
-function getDataFromMlab (username) {
+
+function getDataFromMlabXXXLoadAccountPage (username) {
 	console.log(username);
 	console.log(username);
 	return new Promise((resolve, reject) => {
@@ -114,9 +108,10 @@ function getDataFromMlab (username) {
 		})
 		.then((response)=>{
 			console.log(response[0].username);			
-			sessionStorage.setItem('username', response[0].username);
-			sessionStorage.setItem('firstName', response[0].firstName);
-			sessionStorage.setItem('lastName', response[0].lastName);
+			localStorage.setItem('username', response[0].username);
+			localStorage.setItem('firstName', response[0].firstName);
+			localStorage.setItem('lastName', response[0].lastName);
+			location.href=('accountPage.html');
 			resolve();
 		});
 		// .reject((err) => {
@@ -127,10 +122,71 @@ function getDataFromMlab (username) {
 }
 
 function logOut() {
-	$('.logOut').click(function(){
+	$('button.logOut').click(function(){
+		localStorage.clear();
+		location.href='index.html';
 
 	})
 }
+
+
+//function to delete account
+//1) make a div show up with a superDelete button and asks if user is sure
+//2) make everything else go blurry
+//3) pressing the button will delete the mLab collection for that user
+function deleteAccount() {
+	console.log(localStorage);
+	$('button.deleteAccount').click(function(){
+		console.log('trying to delete');
+		$('main').append(`
+			<div class='superDelete'>
+				<p>Are you sure?</p>
+				<button class='kill'>Yep</button>
+				<button class='reprieve'>Nep</button>
+			</div>`);
+		$('button.kill').click(function(){
+			console.log('here I go killing again');
+			return new Promise((resolve, reject)=>{
+				$.ajax({
+				method: "GET",
+				url: `https://api.mlab.com/api/1/databases/mobius/collections/hoomen?q=
+				{'username':'${localStorage.username}'}&apiKey=${mLabApiKey}`
+				})
+				.then((response)=>{
+					let dBiD = response[0]._id.$oid;
+					console.log(dBiD);
+					$.ajax({
+						method: "DELETE",
+						url: `https://api.mlab.com/api/1/databases/mobius/collections/hoomen/${dBiD}?apiKey=${mLabApiKey}`,
+						success: function(data){
+							console.log('You are out of the club');
+							$('main').html("Your account has been deleted successfully...returning to beginning");
+							$('p').html('');
+							setTimeout(function(){
+								location.href='index.html';
+							}, 3000);
+						},
+						error: function(xhr, status, err) {
+						}
+					});
+					
+				})
+			});
+			
+		});
+	});
+
+}
+
+
+
+function accountFunctionality(){
+	logOut();
+	deleteAccount();
+}
+
+
+$(accountFunctionality());
 
 //https://api.mlab.com/api/1/databases/mobius/collections/hoomen?q={'username': lightsage88&apiKey=hKWiRGd3vrdDNeevRH9itVTSpdHc_Ldt
 // //for alogin form the success response => {
