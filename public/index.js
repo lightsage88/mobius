9f0f413ab1f25b6365acceb369a868d8ec61dfe6
lightsage88 <adrian.e.rosales@gmail.com>
2017-12-19 03:14:50 -0800

    changed up index.js again

--- public/index.js
+++ public/index.js
@@ -219,7 +219,7 @@ function upDateAccount() {
 
 function loadMainPage() {
 
-if(location.href === 'mainPage.html') {
+if(location.href === 'http://localhost:8080/mainPage.html' || 'https://shrouded-anchorage-29615.herokuapp.com/mainPage.html') {
 		$.ajax({
 			method: "GET",
 			url: "/api/hoomans/char",
